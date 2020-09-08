5ea7d79975d5fe61929e6453d53c3aac230113c1
Dikiy Igor <wildig1014@yandex.ru>
2020-09-09 01:06:21 +0300

    Init

---
+++ warship_manager/config.py
@@ -0,0 +1,13 @@
+from os.path import join
+
+RPS = 0.016
+AREA_WIDTH = 3000
+AREA_HEIGHT = 3000
+ENEMY_COUNT = 10
+ENTITY_PATH = 'entities'
+STATICS_PATH = join(ENTITY_PATH, 'statics')
+SHIPS_PATH = join(ENTITY_PATH, 'dynamics')
+
+REDIS_HOST = 'redis-18580.c56.east-us.azure.cloud.redislabs.com'
+REDIS_PORT = 18580
+REDIS_PASSWORD = 'pisMmjzMap3vTFIccM7F3dYatXkv5ozT'
---
+++ warship_manager/data_bus.py
@@ -0,0 +1,55 @@
+import json
+import time
+
+from aredis import StrictRedis
+
+from warship_manager.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
+
+
+class DataBus:
+    def __init__(self):
+        self._redis = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, password=REDIS_PASSWORD)
+        self.players_state = {}
+        self.pubsub = self._redis.pubsub()
+        self.pubsub_players = self._redis.pubsub()
+
+    async def init_pubsub(self):
+        await self.pubsub.subscribe('game-state')
+
+    def add_player(self, player_name, player_id):
+        player = {
+            'player_id': player_id,
+            'player_name': player_name,
+            'shooting': 0,
+            'angle': 0,
+            'direction': 0
+        }
+        self.players_state[player_id] = player
+
+    def del_player(self, player_id):
+        del self.players_state[player_id]
+
+    def set_moving(self, player_id, angle, direction):
+        self.players_state[player_id]['angle'] = angle
+        self.players_state[player_id]['direction'] = direction
+
+    def set_shooting(self, player_id, shooting):
+        self.players_state[player_id]['shooting'] = shooting
+
+    async def send_state(self):
+        for pl in self.players_state:
+            if self.players_state[pl].get('shooting') == 1:
+                print(time.time())
+        await self.pubsub_players.execute_command("PUBLISH", 'players-state', json.dumps(self.players_state))
+
+    async def get_message(self):
+        raw_message = await self.pubsub.get_message()
+        if raw_message:
+            return self.get_dict(raw_message['data'])
+        return None
+
+    @staticmethod
+    def get_dict(value: bytes):
+        if isinstance(value, bytes):
+            return json.loads(value.decode().replace("'", '"'))
+        return {}
