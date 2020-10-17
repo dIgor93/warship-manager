3abe14519196238cadffcb7ffa12c3f257372c48
Igor Dikiy <igor.dikiy@south.rt.ru>
2020-10-17 23:05:51 +0300

    rolback env

--- front/script/main.js
+++ front/script/main.js
@@ -2,9 +2,9 @@ const SEA_COLOR = "#256692";
 const AREA_WIDTH = 3000;
 const AREA_HEIGHT = 3000;
 let DRAW_BORDERS = false;
-const HOST = 'steel-rebbot-2.herokuapp.com'
-const TEXTURE_URL = `https://${HOST}/load_data`;
-const WS_URL = `wss://${HOST}/ws`;
+const HOST = 'localhost:8000'
+const TEXTURE_URL = `http://${HOST}/load_data`;
+const WS_URL = `ws://${HOST}/ws`;
 
 let action = {up: false, down: false, left: false, right: false, shot: false};
 let last_action = {};
